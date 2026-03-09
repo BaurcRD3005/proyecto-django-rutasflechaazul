fece7fa031f57aeff7561cec12f36aae58f54527
Babu <b84318348@gmail.com>
2026-03-09 15:22:37 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -90,8 +90,9 @@ import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
