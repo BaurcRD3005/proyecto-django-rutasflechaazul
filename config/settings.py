41f7679c11c0a6c6114b6f46d0ed33ff59886d43
Babu <b84318348@gmail.com>
2026-03-18 10:06:36 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
     )
 }
 
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
