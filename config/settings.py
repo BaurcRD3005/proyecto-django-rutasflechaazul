f064dd8d6bac77906cdb34b157e6f95643cc32c0
Babu <b84318348@gmail.com>
2026-03-23 10:41:51 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            engine="django.db.backends.mysql"
+        )
     }
 else:
     # Base de datos local para desarrollo
